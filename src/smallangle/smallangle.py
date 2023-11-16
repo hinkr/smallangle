bc634fdd53af8534a032cc0e228a1dc4991d1dd1
hinkr <hinkedekinga@outlook.com>
2023-11-16 11:42:38 +0100

    Added click and docstrings

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -14,10 +14,15 @@ def main():
     "-n",
     "--number",
     default=10,
-    help="Amount of steps between 0 and 2pi",
+    help="Amount of steps between 0 and 2 pi",
     show_default=True,
 )
 def sin(number):
+    """Make a dataframe of chosen values and their sinus between 0 and 2 pi
+
+    Args:
+        number (int): Amount of steps between 0 and 2 pi
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
@@ -29,13 +34,36 @@ def sin(number):
     "-n",
     "--number",
     default=10,
-    help="Amount of steps between 0 and 2pi",
+    help="Amount of steps between 0 and 2 pi",
     show_default=True,
 )
 def tan(number):
+    """Make dataframe of chosen values and their tangent between 0 and 2 pi
+
+    Args:
+        number (int): Amount of steps between 0 and 2 pi
+    """
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
+    return
+
+
+@main.command()
+@click.argument("epsilon")
+def approx(epsilon):
+    """Find the upper limit for the small-angle approximation for a chosen accuracy
+
+    Args:
+        epsilon (float): accuracy
+    """
+    angle = 0
+    while np.abs(angle - np.sin(angle)) <= float(epsilon):
+        angle += 0.001
+    print(
+        f"For an accuracy of {epsilon},",
+        f"the small-angle approximation holds up to x = {round(angle,3)}",
+    )
     return
 
 
