edd9c8380c50470b80eb104be696844d891d8a80
hinkr <hinkedekinga@outlook.com>
2023-11-16 10:30:50 +0100

    code aangepast voor click

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -1,9 +1,22 @@
 import click
 import numpy as np
-from numpy import pi
 import pandas as pd
+from numpy import pi
 
 
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="Amount of steps between 0 and 2pi",
+    show_default=True,
+)
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
@@ -11,6 +24,14 @@ def sin(number):
     return
 
 
+@main.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="Amount of steps between 0 and 2pi",
+    show_default=True,
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -19,4 +40,4 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    main()
